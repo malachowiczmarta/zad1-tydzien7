90b26e146b620f715299602bb860078aa338320c
Marta Małachowicz <68849042+malachowiczmarta@users.noreply.github.com>
2020-12-09 13:41:24 +0100

    added formated data

--- src/components/User.js
+++ src/components/User.js
@@ -1,18 +1,51 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-// import Moment from "react-moment";
+import { useParams, Link } from "react-router-dom";
+import Moment from "react-moment";
 
 function User({ data }) {
   const { id } = useParams();
   const { results } = data;
+  console.log(results);
   if (!results) {
     return null;
   }
 
-  const matchingUsers = results.filter((user) => user.login.uuid === id);
+  const matchingUsers = results.filter((elem) => elem.login.uuid === id);
   const user = matchingUsers[0];
+  const dateToFormat = user.registered.date;
 
-  return <div>{user && user.name.first}</div>;
+  console.log(dateToFormat);
+  // const {name, location, email} = user;
+
+  return (
+    <>
+      <Link to="/">&larr; Wróć do listy</Link>
+      <div className="user-container">
+        <img src={user.picture.large} alt="profile" />
+        <h2>
+          {user.name.first
+            ? `${user.name.first} ${user.name.last}`
+            : "Unavailable"}
+        </h2>
+        <ul>
+          <li>
+            <b>Address:&nbsp;</b>
+            {user.location
+              ? `${user.location.city} ${user.location.street.name}`
+              : "Unavailable"}
+          </li>
+          <li>
+            <b>Email:&nbsp;</b>
+            {user.email}
+          </li>
+          <li>
+            <b>Registered at:&nbsp;</b>
+            <Moment format="YYYY/MM/DD">{dateToFormat}</Moment>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 
   // return (
   //   <>
